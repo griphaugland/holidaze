9fedc3e8032a38e49c366893614c816e3ad94c2a
griphaugland <griphaugland@gmail.com>
2024-05-18 23:34:22 +0200

    fixed url

--- src/components/modal/modalcontent/Login.jsx
+++ src/components/modal/modalcontent/Login.jsx
@@ -15,7 +15,7 @@ function Login() {
     setError(null);
 
     try {
-      const response = await fetch("https://your-api-url.com/auth/login", {
+      const response = await fetch("https://v2.api.noroff.dev/auth/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
